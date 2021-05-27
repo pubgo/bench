b3714de9d996ed302111052d9dc94855bff7e76e
白云辉 <baiyh@weipaitang.com>
2021-05-27 14:55:26 +0800

    fix: update

--- encode/encode_test.go
+++ encode/encode_test.go
@@ -5,8 +5,12 @@ import (
 	"encoding/gob"
 	"encoding/json"
 	"fmt"
+	_ "github.com/alecthomas/go_serialization_benchmarks"
+	"github.com/ethereum/go-ethereum/rlp"
+	_ "github.com/ethereum/go-ethereum/rlp"
 	jsonitor "github.com/json-iterator/go"
 	"github.com/pubgo/xerror"
+	_ "github.com/smallnest/gosercomp/model"
 	amino "github.com/tendermint/go-amino"
 	msgpack "github.com/vmihailenco/msgpack/v5"
 	"testing"
@@ -19,23 +23,46 @@ var std = jsonitor.Config{
 }.Froze()
 var cdc = amino.NewCodec()
 
-func init() {
-	std = jsonitor.ConfigCompatibleWithStandardLibrary
-	cdc.RegisterConcrete(&Student{}, "main/Student", nil)
+type d interface {
 }
 
-func TestAmino(t *testing.T) {
-	s1 := Student{"张三", 18, "江苏省"}
-	var dt = xerror.PanicBytes(cdc.MarshalJSON(s1))
+func init() {
+	std = jsonitor.ConfigCompatibleWithStandardLibrary
+	std = jsonitor.ConfigDefault
+	cdc.RegisterInterface((*d)(nil), nil)
+	cdc.RegisterConcrete(&Student{}, "main/Student", nil)
+	cdc.RegisterConcrete(&Student1{}, "main/Student1", nil)
+}
+
+//BenchmarkUnmarshalByColfer
+//BenchmarkUnmarshalByZebrapack
+//BenchmarkUnmarshalByGencode
+//BenchmarkUnmarshalByMsgp
+
+//func TestAmino(t *testing.T) {
+//	s1 := getStu()
+//	var dt = xerror.PanicBytes(cdc.MarshalJSON(s1))
+//	fmt.Printf("%s\n", dt)
+//
+//	var val Student
+//	xerror.Panic(cdc.UnmarshalJSON(dt, &val))
+//	fmt.Printf("%#v\n", val)
+//}
+
+func TestRlp(t *testing.T) {
+	s1 := getStu()
+	var dt, err = rlp.EncodeToBytes(s1)
+	xerror.Panic(err)
+
 	fmt.Printf("%s\n", dt)
 
 	var val Student
-	xerror.Panic(cdc.UnmarshalJSON(dt, &val))
+	xerror.Panic(rlp.DecodeBytes(dt, &val))
 	fmt.Printf("%#v\n", val)
 }
 
 func TestMsgpack(t *testing.T) {
-	s1 := Student{"张三", 18, "江苏省"}
+	s1 := getStu()
 	var dt, err = msgpack.Marshal(s1)
 	xerror.Panic(err)
 	fmt.Printf("%s\n", dt)
@@ -46,35 +73,59 @@ func TestMsgpack(t *testing.T) {
 }
 
 func BenchmarkAminoEncode(b *testing.B) {
-	s1 := Student{"张三", 18, "江苏省"}
+	s1 := getStu()
 	for i := 0; i < b.N; i++ {
 		var _, _ = cdc.MarshalBinaryBare(s1)
 	}
 }
 
 func BenchmarkAminoDecode(b *testing.B) {
-	s1 := Student{"张三", 18, "江苏省"}
+	s1 := getStu()
 	var dt, err = cdc.MarshalBinaryBare(s1)
 	xerror.Panic(err)
 
+	b.ResetTimer()
 	var val Student
 	for i := 0; i < b.N; i++ {
 		_ = cdc.UnmarshalBinaryBare(dt, &val)
 	}
 }
 
-func BenchmarkMsgEncode(b *testing.B) {
-	s1 := Student{"张三", 18, "江苏省"}
+func BenchmarkRlpEncode(b *testing.B) {
+	s1 := getStu()
+	for i := 0; i < b.N; i++ {
+		_, _ = rlp.EncodeToBytes(s1)
+	}
+}
+
+func BenchmarkRlpDecode(b *testing.B) {
+	s1 := getStu()
+	var dt, err = rlp.EncodeToBytes(s1)
+	xerror.Panic(err)
+
+	//fmt.Printf("%s\n", dt)
+	b.ResetTimer()
+
+	var val Student
+	for i := 0; i < b.N; i++ {
+		_ = rlp.DecodeBytes(dt, &val)
+	}
+	//fmt.Printf("%#v\n", val)
+}
+
+func BenchmarkMsgPackEncode(b *testing.B) {
+	s1 := getStu()
 	for i := 0; i < b.N; i++ {
 		var _, _ = msgpack.Marshal(s1)
 	}
 }
 
-func BenchmarkMsgDecode(b *testing.B) {
-	s1 := Student{"张三", 18, "江苏省"}
+func BenchmarkMsgPackDecode(b *testing.B) {
+	s1 := getStu()
 	var dt, err = msgpack.Marshal(s1)
 	xerror.Panic(err)
 
+	b.ResetTimer()
 	var val Student
 	for i := 0; i < b.N; i++ {
 		_ = msgpack.Unmarshal(dt, &val)
@@ -82,7 +133,7 @@ func BenchmarkMsgDecode(b *testing.B) {
 }
 
 func TestGobEncode(t *testing.T) {
-	s1 := Student{"张三", 18, "江苏省"}
+	s1 := getStu()
 	_, err := serialize(s1)
 	if err != nil {
 		t.Fatal(err)
@@ -90,7 +141,7 @@ func TestGobEncode(t *testing.T) {
 }
 
 func TestGobDecode(t *testing.T) {
-	s1 := Student{"张三", 18, "江苏省"}
+	s1 := getStu()
 	dt, _ := serialize(s1)
 	fmt.Printf("%s\n", dt)
 
@@ -100,55 +151,61 @@ func TestGobDecode(t *testing.T) {
 	}
 	fmt.Printf("%#v\n", val)
 
-	_, ok := val.(Student)
+	_, ok := val.(*Student)
 	if !ok {
 		t.Fatal("not ok")
 	}
 }
 
 func BenchmarkGobEncode(b *testing.B) {
-	s1 := Student{"张三", 18, "江苏省"}
+	s1 := getStu()
 	for i := 0; i < b.N; i++ {
 		_, _ = serialize(s1)
 	}
 }
 
 func BenchmarkGobDecode(b *testing.B) {
-	s1 := Student{"张三", 18, "江苏省"}
+	s1 := getStu()
 	var dt, _ = serialize(s1)
+
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_, _ = deserialize(dt)
 	}
 }
 
 func BenchmarkJsonEncode(b *testing.B) {
-	s1 := Student{"张三", 18, "江苏省"}
+	s1 := getStu()
 	for i := 0; i < b.N; i++ {
 		_, _ = json.Marshal(s1)
 	}
 }
 
 func BenchmarkJsonDecode(b *testing.B) {
-	s1 := Student{"张三", 18, "江苏省"}
+	s1 := getStu()
 	var dd interface{}
 	var dt, _ = json.Marshal(s1)
+
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = json.Unmarshal(dt, &dd)
 	}
 }
 
-func BenchmarkJsonxEncode(b *testing.B) {
-	s1 := Student{"张三", 18, "江苏省"}
+func BenchmarkJsonitorEncode(b *testing.B) {
+	s1 := getStu()
 	for i := 0; i < b.N; i++ {
 		_, _ = std.Marshal(s1)
 
 	}
 }
 
-func BenchmarkJsonxDecode(b *testing.B) {
-	s1 := Student{"张三", 18, "江苏省"}
+func BenchmarkJsonitorDecode(b *testing.B) {
+	s1 := getStu()
 	var dd interface{}
 	var dt, _ = std.Marshal(s1)
+
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = std.Unmarshal(dt, &dd)
 	}
@@ -157,7 +214,16 @@ func BenchmarkJsonxDecode(b *testing.B) {
 type Student struct {
 	Name    string
 	Age     uint8
-	Address string
+	Address []string
+	Data    string
+	Stu     *Student1
+}
+
+type Student1 struct {
+	Name    string
+	Age     uint8
+	Address []string
+	//Data    map[string]interface{}
 }
 
 func serialize(value interface{}) ([]byte, error) {
@@ -184,4 +250,28 @@ func deserialize(valueBytes []byte) (interface{}, error) {
 	}
 
 	return value, nil
+}
+
+func getStu() *Student {
+	return &Student{
+		Name:    "张三",
+		Age:     100,
+		Address: []string{"张三", "张三", "张三", "张三", "张三"},
+		//Data: map[string]string{
+		//	"张三":  "1",
+		//	"张三2": "nil",
+		//	"张三3": "sss",
+		//},
+		Data: "hello",
+		Stu: &Student1{
+			Name:    "张三",
+			Age:     100,
+			Address: []string{"张三", "张三", "张三", "张三", "张三"},
+			//Data: map[string]interface{}{
+			//	"张三":  1,
+			//	"张三2": nil,
+			//	"张三3": "sss",
+			//},
+		},
+	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/pubgo/bench
 go 1.16
 
 require (
+	github.com/alecthomas/go_serialization_benchmarks v0.0.0-20210425181318-cc8354ec00b8
+	github.com/ethereum/go-ethereum v1.9.3
 	github.com/json-iterator/go v1.1.11
 	github.com/pubgo/xerror v0.4.1
+	github.com/smallnest/gosercomp v0.0.0-20210420121053-c12d01245b1d
 	github.com/tendermint/go-amino v0.16.0
 	github.com/valyala/fastrand v1.0.0
 	github.com/vmihailenco/msgpack/v5 v5.3.2
